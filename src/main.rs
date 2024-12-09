c7275615cbf605295d7aeafd4635cbf8bf06856a
Jason Pena <jasonpena@awkless.com>
2024-12-08 22:04:50 -0700

    feat: improve `main` with better skeleton to run binary
    The `main` function now initializes a global logger to receive log output, and
    executes a new function named `run` to run the `ocd` binary. The `main` function
    catches any `ExitCode` to exit with `ExitCode::Success` status, or print any
    error with a backtrace then exit with `ExitCode::Failure` status.
    
    The `run` function is currently just a basic skeleton. It is expected to be
    given a set of `OsString` arguments to parse and use for running the `ocd`
    binary properly. Logic will be added in the future once core functionality of
    the `ocd` binary is coded in.
    
    Signed-off-by: Jason Pena <jasonpena@awkless.com>

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,53 @@
 // SPDX-FileCopyrightText: 2024 Jason Pena <jasonpena@awkless.com>
 // SPDX-License-Identifier: MIT
 
+use env_logger::Builder as EnvLogBuilder;
+use log::{error, LevelFilter};
+use std::{env::args_os, process::exit, ffi::OsString};
+use snafu::{ErrorCompat, Whatever};
+
 fn main() {
-    println!("hello world!");
+    EnvLogBuilder::new()
+        .format_target(false)
+        .format_timestamp(None)
+        .filter_level(LevelFilter::max())
+        .format_indent(Some(8))
+        .init();
+
+    let code = match run(args_os) {
+        Ok(code) => code,
+        Err(err) => {
+            error!("{err}");
+            if let Some(bt) = ErrorCompat::backtrace(&err) {
+                error!("{bt}");
+            }
+            ExitCode::Failure
+        }
+    }
+    .into();
+
+    exit(code);
+}
+
+fn run<I, F>(args: F) -> Result<ExitCode, Whatever>
+where
+    I: IntoIterator<Item = OsString>,
+    F: FnOnce() -> I + Clone,
+{
+    todo!();
+}
+
+#[derive(Debug)]
+pub enum ExitCode {
+    Success,
+    Failure,
+}
+
+impl From<ExitCode> for i32 {
+    fn from(code: ExitCode) -> Self {
+        match code {
+            ExitCode::Success => 0,
+            ExitCode::Failure => 1,
+        }
+    }
 }
