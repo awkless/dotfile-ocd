aab34c627fe184aa4c1ddf158b621b1993290a46
Jason Pena <jasonpena@awkless.com>
2024-12-13 14:45:24 -0700

    feat: implement `RepoSettings::from`
    Now code base can deserialize TOML entry data into `RepoSettings` using `from`
    method. This method will deserialze both `RepoSettings` and `BootstrapSettings`
    into the `bootstrap` field if available in entry.
    
    Signed-off-by: Jason Pena <jasonpena@awkless.com>

--- src/settings.rs
+++ src/settings.rs
@@ -1,8 +1,14 @@
 // SPDX-FileCopyrightText: 2024 Jason Pena <jasonpena@awkless.com>
 // SPDX-License-Identifier: MIT
 
-use std::path::PathBuf;
-use toml_edit::{Key, Item};
+use std::{
+    fmt::{Display, Formatter, Result as FmtResult},
+    path::PathBuf,
+};
+use toml_edit::{
+    visit::{visit_table_like_kv, Visit},
+    Item, Key,
+};
 
 #[derive(Debug, Default, Eq, PartialEq, Clone)]
 pub struct RepoSettings {
@@ -23,8 +29,7 @@ impl RepoSettings {
             name: name.into(),
             branch: branch.into(),
             remote: remote.into(),
-            worktree: Default::default(),
-            bootstrap: Default::default(),
+            ..Default::default()
         }
     }
 
@@ -39,9 +44,34 @@ impl RepoSettings {
     }
 }
 
+fn from_toml<'toml>(entry: (&'toml Key, &'toml Item)) -> RepoSettings {
+    let (key, value) = entry;
+    let mut repo = RepoSettings { name: key.get().into(), ..Default::default() };
+    repo.visit_item(value);
+    repo
+}
+
 impl<'toml> From<(&'toml Key, &'toml Item)> for RepoSettings {
     fn from(entry: (&'toml Key, &'toml Item)) -> Self {
-        todo!();
+        from_toml(entry)
+    }
+}
+
+impl<'toml> Visit<'toml> for RepoSettings {
+    fn visit_table_like_kv(&mut self, key: &'toml str, node: &'toml Item) {
+        match key {
+            "branch" => self.branch = node.as_str().unwrap_or("master").into(),
+            "remote" => self.remote = node.as_str().unwrap_or("origin").into(),
+            "worktree" => self.worktree = node.as_str().map(|s| s.into()),
+            "bootstrap" => {
+                let mut bootstrap = BootstrapSettings::default();
+                bootstrap.visit_item(node);
+                self.bootstrap = Some(bootstrap);
+            }
+            &_ => visit_table_like_kv(self, key, node),
+        };
+
+        visit_table_like_kv(self, key, node);
     }
 }
 
@@ -57,14 +87,7 @@ pub struct BootstrapSettings {
 
 impl BootstrapSettings {
     pub fn new(url: impl Into<String>) -> Self {
-        Self {
-            clone: url.into(),
-            os: Default::default(),
-            depends: Default::default(),
-            ignores: Default::default(),
-            users: Default::default(),
-            hosts: Default::default(),
-        }
+        Self { clone: url.into(), ..Default::default() }
     }
 
     pub fn with_os(mut self, kind: OsKind) -> Self {
@@ -101,6 +124,38 @@ impl BootstrapSettings {
     }
 }
 
+impl<'toml> Visit<'toml> for BootstrapSettings {
+    fn visit_table_like_kv(&mut self, key: &'toml str, node: &'toml Item) {
+        match key {
+            "clone" => self.clone = node.as_str().unwrap_or_default().into(),
+            "os" => self.os = node.as_str().map(|k| OsKind::from(k)),
+            "depends" => {
+                self.depends = node.as_array().map(|a| {
+                    a.into_iter().map(|s| s.as_str().unwrap_or_default().to_string()).collect()
+                })
+            }
+            "ignores" => {
+                self.ignores = node.as_array().map(|a| {
+                    a.into_iter().map(|s| s.as_str().unwrap_or_default().to_string()).collect()
+                })
+            }
+            "users" => {
+                self.users = node.as_array().map(|a| {
+                    a.into_iter().map(|s| s.as_str().unwrap_or_default().to_string()).collect()
+                })
+            }
+            "hosts" => {
+                self.hosts = node.as_array().map(|a| {
+                    a.into_iter().map(|s| s.as_str().unwrap_or_default().to_string()).collect()
+                })
+            }
+            &_ => visit_table_like_kv(self, key, node),
+        };
+
+        visit_table_like_kv(self, key, node);
+    }
+}
+
 #[derive(Debug, Default, Eq, PartialEq, Clone)]
 pub enum OsKind {
     #[default]
@@ -113,15 +168,38 @@ pub enum OsKind {
     Windows,
 }
 
+impl From<&str> for OsKind {
+    fn from(data: &str) -> Self {
+        match data {
+            "any" => Self::Any,
+            "unix" => Self::Unix,
+            "macos" => Self::MacOs,
+            "windows" => Self::Windows,
+            &_ => Self::Any,
+        }
+    }
+}
+
+impl Display for OsKind {
+    fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
+        match self {
+            OsKind::Any => write!(f, "any"),
+            OsKind::Unix => write!(f, "unix"),
+            OsKind::MacOs => write!(f, "macos"),
+            OsKind::Windows => write!(f, "windows"),
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    use indoc::indoc;
     use pretty_assertions::assert_eq;
     use rstest::{fixture, rstest};
     use snafu::report;
     use toml_edit::{DocumentMut, TomlError};
-    use indoc::indoc;
 
     #[fixture]
     fn repo_settings_doc() -> Result<DocumentMut, TomlError> {
@@ -131,9 +209,13 @@ mod tests {
             remote = "origin"
             worktree = "$HOME"
 
+            [foo.should_ignore_this]
+            clone = "https://some/url"
+            os = "unix"
+
             [bar]
             branch = "main"
-            remote = "origin"
+            remote = "upstream"
             worktree = "$HOME"
 
             [bar.bootstrap]
@@ -155,7 +237,7 @@ mod tests {
             .with_worktree("$HOME")
     )]
     #[case::with_bootstrap(
-        RepoSettings::new("bar", "main", "origin")
+        RepoSettings::new("bar", "main", "upstream")
             .with_worktree("$HOME")
             .with_bootstrap(
                 BootstrapSettings::new("https://some/url")
