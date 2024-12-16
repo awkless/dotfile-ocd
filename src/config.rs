32159b339ebefa01edadc17cef7367b785bdf66e
Jason Pena <jasonpena@awkless.com>
2024-12-16 14:22:23 -0700

    feat: implement `ConfigFile::load`
    Signed-off-by: Jason Pena <jasonpena@awkless.com>

--- src/config.rs
+++ src/config.rs
@@ -3,28 +3,21 @@
 
 use crate::{
     locate::Locator,
-    toml::Toml,
+    toml::{Toml, TomlError},
     settings::{RepoSettings, CmdHookSettings},
 };
 
 use snafu::prelude::*;
 use std::{
-    path::Path,
+    path::{PathBuf, Path},
     fmt::{Display, Formatter, Result as FmtResult},
-
+    fs::OpenOptions,
+    io::{Read, Write, Error as IoError},
 };
 use log::debug;
 use mkdirp::mkdirp;
 
-pub trait Config {
-    type Entry;
-
-    fn get(&self, doc: &Toml, key: &str) -> Result<Self::Entry, ConfigError>;
-    fn add(&self, doc: &Toml, entry: Self::Entry) -> Result<Option<Self::Entry>, ConfigError>;
-    fn remove(&self, doc: &Toml, key: &str) -> Result<Self::Entry, ConfigError>;
-    fn location<'cfg>(&self, locator: &'cfg impl Locator) -> &'cfg Path;
-}
-
+/// Format preserving configuration file handler.
 #[derive(Clone, Debug)]
 pub struct ConfigFile<'cfg, C, L>
 where
@@ -41,8 +34,34 @@ where
     C: Config,
     L: Locator,
 {
+    /// Load new configuration file.
+    ///
+    /// If path to configuration file does not exist, then it will created at
+    /// target location. Otherwise, configuration file will be read and parsed
+    /// like normal.
+    ///
+    /// # Errors
+    ///
+    /// Will fail if parent directory cannot be created when needed, or
+    /// configuration file cannot be opened, read, and/or parsed at all.
     pub fn load(config: C, locator: &'cfg L) -> Result<Self, ConfigError> {
-        todo!();
+        let path = config.location(locator);
+        debug!("Load new configuration file from '{}'", path.display());
+        let root = path.parent().unwrap();
+        mkdirp(root).context(MakeDirPSnafu { path: root.to_path_buf() })?;
+
+        let mut file = OpenOptions::new()
+            .write(true)
+            .truncate(false)
+            .read(true)
+            .create(true)
+            .open(path)
+            .context(FileOpenSnafu { path: path.to_path_buf() })?;
+        let mut buffer = String::new();
+        file.read_to_string(&mut buffer).context(FileReadSnafu { path: path.to_path_buf() })?;
+        let doc: Toml = buffer.parse().context(TomlSnafu { path: path.to_path_buf() })?;
+
+        Ok(Self { doc, config, locator })
     }
 }
 
@@ -56,6 +75,17 @@ where
     }
 }
 
+/// Configuration file startegy.
+///
+/// Interface to simplify serialization and deserialization of parsed TOML data.
+pub trait Config {
+    type Entry;
+
+    fn get(&self, doc: &Toml, key: &str) -> Result<Self::Entry, ConfigError>;
+    fn add(&self, doc: &Toml, entry: Self::Entry) -> Result<Option<Self::Entry>, ConfigError>;
+    fn remove(&self, doc: &Toml, key: &str) -> Result<Self::Entry, ConfigError>;
+    fn location<'cfg>(&self, locator: &'cfg impl Locator) -> &'cfg Path;
+}
 
 #[derive(Clone, Debug, Default, PartialEq, Eq)]
 pub struct RepoConfig;
@@ -76,7 +106,7 @@ impl Config for RepoConfig {
     }
 
     fn location<'cfg>(&self, locator: &'cfg impl Locator) -> &'cfg Path {
-        todo!();
+        locator.repos_config()
     }
 }
 
@@ -99,17 +129,31 @@ impl Config for CmdHookConfig {
     }
 
     fn location<'cfg>(&self, locator: &'cfg impl Locator) -> &'cfg Path {
-        todo!();
+        locator.hooks_config()
     }
 }
 
-#[derive(Debug, Snafu, PartialEq, Eq)]
+/// Configuration error type for public API.
+#[derive(Debug, Snafu)]
 pub struct ConfigError(InnerConfigError);
 
+/// Alias to allow one-off functions with different error type.
 pub type Result<T, E = ConfigError> = std::result::Result<T, E>;
 
-#[derive(Debug, Snafu, PartialEq, Eq)]
-enum InnerConfigError {}
+#[derive(Debug, Snafu)]
+enum InnerConfigError {
+    #[snafu(display("Failed to create parent path to '{}'", path.display()))]
+    MakeDirP { path: PathBuf, source: IoError },
+
+    #[snafu(display("Failed to open '{}'", path.display()))]
+    FileOpen { path: PathBuf, source: IoError },
+
+    #[snafu(display("Failed to read '{}'", path.display()))]
+    FileRead { path: PathBuf, source: IoError },
+
+    #[snafu(display("Failed to parse '{}'", path.display()))]
+    Toml { path: PathBuf, source: TomlError },
+}
 
 #[cfg(test)]
 mod tests {
@@ -119,6 +163,7 @@ mod tests {
         locate::MockLocator,
     };
 
+    use pretty_assertions::assert_eq;
     use snafu::{report, Whatever};
     use rstest::{fixture, rstest};
     use indoc::indoc;
@@ -171,7 +216,8 @@ mod tests {
         let config_dir = config_dir?;
         let fixture = config_dir.get("config.toml")?;
         let mut locator = MockLocator::new();
-        locator.expect_config_dir().return_const(config_dir.as_path().into());
+        locator.expect_repos_config().return_const(fixture.as_path().into());
+        locator.expect_hooks_config().return_const(fixture.as_path().into());
 
         let config = ConfigFile::load(config_kind, &locator)
             .with_whatever_context(|_| "Failed to load configuration file")?;
