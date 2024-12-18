81d1735812935e65f04628e7c5d0ccd5cf839b68
Jason Pena <jasonpena@awkless.com>
2024-12-18 14:08:14 -0700

    refactor!: move locator into config strategy
    Config strategies can now allow for the caller to determine the name of config
    files and use `Locator` implementations directly through setter dependency
    injection to construct absolute paths for each config file per strategy.
    
    Signed-off-by: Jason Pena <jasonpena@awkless.com>

--- src/config.rs
+++ src/config.rs
@@ -19,20 +19,17 @@ use mkdirp::mkdirp;
 
 /// Format preserving configuration file handler.
 #[derive(Clone, Debug)]
-pub struct ConfigFile<'cfg, C, L>
+pub struct ConfigFile<C>
 where
     C: Config,
-    L: Locator,
 {
     doc: Toml,
     config: C,
-    locator: &'cfg L,
 }
 
-impl<'cfg, C, L> ConfigFile<'cfg, C, L>
+impl<C> ConfigFile<C>
 where
     C: Config,
-    L: Locator,
 {
     /// Load new configuration file.
     ///
@@ -44,8 +41,8 @@ where
     ///
     /// Will fail if parent directory cannot be created when needed, or
     /// configuration file cannot be opened, read, and/or parsed at all.
-    pub fn load(config: C, locator: &'cfg L) -> Result<Self, ConfigError> {
-        let path = config.location(locator);
+    pub fn load(config: C) -> Result<Self, ConfigError> {
+        let path = config.as_path();
         debug!("Load new configuration file from '{}'", path.display());
         let root = path.parent().unwrap();
         mkdirp(root).context(MakeDirPSnafu { path: root.to_path_buf() })?;
@@ -59,21 +56,20 @@ where
             .context(FileOpenSnafu { path: path.to_path_buf() })?;
         let mut buffer = String::new();
         file.read_to_string(&mut buffer).context(FileReadSnafu { path: path.to_path_buf() })?;
-        let doc: Toml = buffer.parse().context(TomlSnafu { path: path.to_path_buf() })?;
+        let doc = buffer.parse().context(TomlSnafu { path: path.to_path_buf() })?;
 
-        Ok(Self { doc, config, locator })
+        Ok(Self { doc, config })
     }
 
     /// Coerces to a [`Path`] slice.
     pub fn as_path(&self) -> &Path {
-        self.config.location(self.locator)
+        self.config.as_path()
     }
 }
 
-impl<'cfg, C, L> Display for ConfigFile<'cfg, C, L>
+impl<C> Display for ConfigFile<C>
 where
     C: Config,
-    L: Locator,
 {
     fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
         write!(f, "{}", self.doc)
@@ -89,11 +85,20 @@ pub trait Config: Debug {
     fn get(&self, doc: &Toml, key: &str) -> Result<Self::Entry, ConfigError>;
     fn add(&self, doc: &Toml, entry: Self::Entry) -> Result<Option<Self::Entry>, ConfigError>;
     fn remove(&self, doc: &Toml, key: &str) -> Result<Self::Entry, ConfigError>;
-    fn location<'cfg>(&self, locator: &'cfg impl Locator) -> &'cfg Path;
+    fn with_locator<'cfg>(&mut self, locator: &'cfg impl Locator);
+    fn as_path(&self) -> &Path;
 }
 
-#[derive(Clone, Debug, Default, PartialEq, Eq)]
-pub struct RepoConfig;
+#[derive(Clone, Default, Debug)]
+pub struct RepoConfig {
+    path: PathBuf,
+}
+
+impl RepoConfig {
+    pub fn new(path: impl Into<PathBuf>) -> Self {
+        Self { path: path.into() }
+    }
+}
 
 impl Config for RepoConfig {
     type Entry = RepoSettings;
@@ -110,13 +115,25 @@ impl Config for RepoConfig {
         todo!();
     }
 
-    fn location<'cfg>(&self, locator: &'cfg impl Locator) -> &'cfg Path {
-        locator.repos_config()
+    fn as_path(&self) -> &Path {
+        &self.path
+    }
+
+    fn with_locator<'cfg>(&mut self, locator: &'cfg impl Locator) {
+        self.path = locator.config_dir().join(&self.path);
     }
 }
 
-#[derive(Clone, Debug, Default, PartialEq, Eq)]
-pub struct CmdHookConfig;
+#[derive(Clone, Default, Debug)]
+pub struct CmdHookConfig {
+    path: PathBuf,
+}
+
+impl CmdHookConfig {
+    pub fn new(path: impl Into<PathBuf>) -> Self {
+        Self { path: path.into() }
+    }
+}
 
 impl Config for CmdHookConfig {
     type Entry = CmdHookSettings;
@@ -133,8 +150,12 @@ impl Config for CmdHookConfig {
         todo!();
     }
 
-    fn location<'cfg>(&self, locator: &'cfg impl Locator) -> &'cfg Path {
-        locator.hooks_config()
+    fn as_path(&self) -> &Path {
+        &self.path
+    }
+
+    fn with_locator<'cfg>(&mut self, locator: &'cfg impl Locator) {
+        self.path = locator.config_dir().join(&self.path);
     }
 }
 
@@ -176,16 +197,22 @@ mod tests {
     #[fixture]
     fn config_dir() -> Result<FixtureHarness, Whatever> {
         let harness = FixtureHarness::open()?
-            .with_file("config.toml", |fixture| {
+            .with_file("repos.toml", |fixture| {
                 fixture
                     .data(indoc! {r#"
                         # Formatting should remain the same!
-
                         [repos.vim]
                         branch = "master"
                         remote = "origin"
                         workdir_home = true
-
+                    "#})
+                    .kind(FileKind::Normal)
+                    .write()
+            })?
+            .with_file("hooks.toml", |fixture| {
+                fixture
+                    .data(indoc! {r#"
+                        # Formatting should remain the same!
                         [hooks]
                         commit = [
                             { pre = "hook.sh", post = "hook.sh", workdir = "/some/path" },
@@ -204,19 +231,20 @@ mod tests {
 
     #[report]
     #[rstest]
-    #[case::repo_config(RepoConfig)]
-    #[case::cmd_hook_config(CmdHookConfig)]
+    #[case::repo_config(RepoConfig::new("repos.toml"), "repos.toml")]
+    #[case::cmd_hook_config(CmdHookConfig::new("hooks.toml"), "hooks.toml")]
     fn config_file_load_parse_file(
         config_dir: Result<FixtureHarness, Whatever>,
-        #[case] config_kind: impl Config,
+        #[case] mut config_strat: impl Config,
+        #[case] fixture_name: &str,
     ) -> Result<(), Whatever> {
         let config_dir = config_dir?;
-        let fixture = config_dir.get("config.toml")?;
+        let fixture = config_dir.get(fixture_name)?;
         let mut locator = MockLocator::new();
-        locator.expect_repos_config().return_const(fixture.as_path().into());
-        locator.expect_hooks_config().return_const(fixture.as_path().into());
+        locator.expect_config_dir().return_const(config_dir.as_path().into());
+        config_strat.with_locator(&locator);
 
-        let config = ConfigFile::load(config_kind, &locator)
+        let config = ConfigFile::load(config_strat)
             .with_whatever_context(|_| "Failed to load configuration file")?;
         assert_eq!(config.to_string(), fixture.as_str());
 
@@ -225,18 +253,18 @@ mod tests {
 
     #[report]
     #[rstest]
-    #[case::repo_config(RepoConfig)]
-    #[case::hook_cmd_config(CmdHookConfig)]
+    #[case::repo_config(RepoConfig::new("repos.toml"))]
+    #[case::hook_cmd_config(CmdHookConfig::new("hooks.toml"))]
     fn config_file_load_create_new_file(
         config_dir: Result<FixtureHarness, Whatever>,
-        #[case] config_kind: impl Config,
+        #[case] mut config_strat: impl Config,
     ) -> Result<(), Whatever> {
         let config_dir = config_dir?;
         let mut locator = MockLocator::new();
-        locator.expect_repos_config().return_const(config_dir.as_path().join("repos.toml"));
-        locator.expect_hooks_config().return_const(config_dir.as_path().join("hooks.toml"));
+        locator.expect_config_dir().return_const(config_dir.as_path().into());
+        config_strat.with_locator(&locator);
 
-        let config = ConfigFile::load(config_kind, &locator)
+        let config = ConfigFile::load(config_strat)
             .with_whatever_context(|_| "Failed to load configuration file")?;
         assert!(config.as_path().exists());
 
@@ -245,19 +273,18 @@ mod tests {
 
     #[report]
     #[rstest]
-    #[case::repo_config(RepoConfig)]
-    #[case::cmd_hook_config(CmdHookConfig)]
+    #[case::repo_config(RepoConfig::new("bad_format.toml"))]
+    #[case::cmd_hook_config(CmdHookConfig::new("bad_format.toml"))]
     fn config_file_load_return_err_toml(
         config_dir: Result<FixtureHarness, Whatever>,
-        #[case] config_kind: impl Config,
+        #[case] mut config_strat: impl Config,
     ) -> Result<(), Whatever> {
         let config_dir = config_dir?;
-        let fixture = config_dir.get("bad_format.toml")?;
         let mut locator = MockLocator::new();
-        locator.expect_repos_config().return_const(fixture.as_path().into());
-        locator.expect_hooks_config().return_const(fixture.as_path().into());
+        locator.expect_config_dir().return_const(config_dir.as_path().into());
+        config_strat.with_locator(&locator);
 
-        let result = ConfigFile::load(config_kind, &locator);
+        let result = ConfigFile::load(config_strat);
         assert!(matches!(result.unwrap_err().0, InnerConfigError::Toml { .. }));
 
         Ok(())
