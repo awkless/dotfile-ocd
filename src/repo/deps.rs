12984f1c027aeb245492b4a46413bc0592ed64a3
Jason Pena <jasonpena@awkless.com>
2024-12-26 20:38:25 -0700

    feat: add `Dependencies` constructor with helper routines
    Signed-off-by: Jason Pena <jasonpena@awkless.com>

--- src/repo/deps.rs
+++ src/repo/deps.rs
@@ -1,6 +1,8 @@
 // SPDX-FileCopyrightText: 2024 Jason Pena <jasonpena@awkless.com>
 // SPDX-License-Identifier: MIT
 
+use crate::config::{ConfigFile, RepoConfig, Locator, RepoSettings};
+
 use std::collections::HashMap;
 
 /// Handle repository dependencies.
@@ -19,16 +21,31 @@ pub struct Dependencies {
 }
 
 impl Dependencies {
+    /// Construct new dependency handler.
     pub fn new() -> Self {
-        todo!();
+        Self { adj_list: HashMap::new() }
     }
 
-    pub fn add_vertex(&mut self, vertex: impl Into<String>) {
-        todo!();
+    /// Load configuration file dependencies.
+    pub fn with_config_file(&mut self, config: &ConfigFile<'_, RepoConfig, impl Locator>) {
+        for repo in config.iter() {
+            self.add_vertex(repo.name.clone());
+            let deps = repo.bootstrap.as_ref().and_then(|b| b.depends.clone()).unwrap_or_default();
+            for dep in deps {
+                self.add_edge(repo.name.clone(), dep.clone());
+            }
+        }
     }
 
-    pub fn add_edge(&mut self, vertex: impl Into<String>, edge: impl Into<String>) {
-        todo!();
+    /// Add new vertex.
+    pub fn add_vertex(&mut self, vertex: String) {
+        self.adj_list.entry(vertex.clone()).or_default();
+    }
+
+    /// Add new edge to given vertex.
+    pub fn add_edge(&mut self, vertex: String, edge: String) {
+        self.adj_list.entry(vertex.clone()).or_default().push(edge.clone());
+        self.adj_list.entry(edge.clone()).or_default();
     }
 
     pub fn topological_sort(&self) -> Option<Vec<String>> {
