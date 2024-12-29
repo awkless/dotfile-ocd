f0f38e93e3959e291b01b0ca456ce35a5685c0b2
Jason Pena <jasonpena@awkless.com>
2024-12-29 09:31:09 -0700

    feat!: define full CLI command set
    Signed-off-by: Jason Pena <jasonpena@awkless.com>

--- src/cli.rs
+++ src/cli.rs
@@ -9,7 +9,10 @@ pub use ctx::*;
 use clap::{Args, Parser, Subcommand, Error as ClapError};
 use clap_verbosity_flag::{InfoLevel, Verbosity};
 use snafu::prelude::*;
-use std::ffi::OsString;
+use std::{
+    path::PathBuf,
+    ffi::OsString,
+};
 
 #[derive(Debug, Parser)]
 #[command(
@@ -45,7 +48,121 @@ impl Cli {
 }
 
 #[derive(Debug, Subcommand)]
-pub enum CommandSet { }
+pub enum CommandSet {
+    /// Initialize new repository.
+    Init(InitOptions),
+
+    /// Clone new repository.
+    Clone(CloneOptions),
+
+    /// Remove repository from collection.
+    Remove(RemoveOptions),
+
+    /// Deploy target repositories.
+    Deploy(DeployOptions),
+
+    /// Undeploy target repositories.
+    Undeploy(UndeployOptions),
+
+    /// List current set of repositories.
+    List(ListOptions),
+
+    /// Show status of repositories.
+    Status(StatusOptions),
+
+    /// Pull changes to all repositories.
+    Pull(PullOptions),
+
+    /// Push changes from all repositories.
+    Push(PushOptions),
+
+    /// Commit changes to all repositories.
+    Commit(CommitOptions),
+
+    /// Run user's Git binary on target repository.
+    #[command(external_subcommand)]
+    Git(Vec<OsString>),
+}
+
+#[derive(Args, Debug)]
+pub struct InitOptions {
+    pub name: String,
+
+    #[arg(short, long, value_name = "DIR")]
+    pub bare_alias: Option<PathBuf>,
+
+    #[arg(short, long, value_name = "BRANCH")]
+    pub branch: Option<String>,
+}
+
+#[derive(Args, Debug)]
+pub struct CloneOptions {
+    /// Remove to clone from.
+    pub remote: String,
+
+    /// Set name of cloned repository.
+    pub repo: Option<String>,
+}
+
+#[derive(Args, Debug)]
+pub struct RemoveOptions {
+    /// Remove to clone from.
+    pub remote: String,
+}
+
+#[derive(Args, Debug)]
+pub struct DeployOptions {
+    #[arg(num_args = 1.., value_delimiter = ',')]
+    pub repos: Vec<String>,
+}
+
+#[derive(Args, Debug)]
+pub struct UndeployOptions {
+    #[arg(num_args = 1.., value_delimiter = ',')]
+    pub repos: Vec<String>,
+}
+
+#[derive(Args, Debug)]
+pub struct ListOptions {
+    #[arg(default_value_t = ListAction::default(), long, short, value_enum, value_name = "ACTION")]
+    pub show: ListAction,
+}
+
+#[derive(Args, Debug)]
+pub struct StatusOptions {
+    /// Give a short status report.
+    #[arg(long, short)]
+    pub terse: bool,
+}
+
+#[derive(Args, Debug)]
+pub struct PullOptions {
+    /// Target remote to push to.
+    pub remote: Option<String>,
+
+    /// Target branch to push to.
+    pub branch: Option<String>,
+}
+
+#[derive(Args, Debug)]
+pub struct PushOptions {
+    /// Target remote to push to.
+    pub remote: Option<String>,
+
+    /// Target branch to push to.
+    pub branch: Option<String>,
+}
+
+#[derive(Args, Debug)]
+pub struct CommitOptions {
+    /// Amend or reword current commit.
+    #[arg(long, short, value_name = "ACTION", value_enum)]
+    pub fixup: Option<FixupAction>,
+
+    /// Use MSG as the commit messsage.
+    #[arg(long, short, value_name = "MSG")]
+    pub message: Option<String>,
+}
 
 #[derive(Debug, Args)]
 #[command(next_help_heading = "Command Options")]
