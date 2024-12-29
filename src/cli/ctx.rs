df59eeb55d27c70d6cd7f18f0e608eed74328b76
Jason Pena <jasonpena@awkless.com>
2024-12-29 09:31:09 -0700

    feat!: define full CLI command set
    Signed-off-by: Jason Pena <jasonpena@awkless.com>

--- src/cli/ctx.rs
+++ src/cli/ctx.rs
@@ -16,3 +16,28 @@ pub enum HookAction {
     /// Never execute hooks no questions asked.
     Never,
 }
+
+/// Behavior types for list command.
+#[derive(Default, Copy, Clone, Debug, PartialEq, Eq, ValueEnum)]
+pub enum ListAction {
+    /// List full collection of repositories.
+    #[default]
+    All,
+
+    /// Only list repositories that have been deployed.
+    Deployed,
+
+    /// Only list repositories that have not been deployed.
+    Undeployed,
+}
+
+/// Fixup actions for `--fixup` flag in commit command.
+#[derive(Default, Copy, Clone, Debug, PartialEq, Eq, ValueEnum)]
+pub enum FixupAction {
+    /// Amend changes in latest commit to all repositories.
+    #[default]
+    Amend,
+
+    /// Fix text of latest commit to all repositories.
+    Reword,
+}
